be800fc2a1871ec04a4fafee1fafe56676a8e6d0
Alexander Brandborg <alexander.brandborg@hotmail.com>
2025-05-06 18:13:02 +0200

    clean up sun and moon

--- solutions/sun_and_moon.py
+++ solutions/sun_and_moon.py
@@ -3,19 +3,19 @@
 def main():
     line1 = input().split()
     current_sun_year = int(line1[0])
-    sun_cycle_years = int(line1[1])
+    years_in_sun_cycle = int(line1[1])
 
     line2 = input().split()
     current_moon_year = int(line2[0])
-    moon_cycle_years = int(line2[1])
+    years_in_moon_cycle = int(line2[1])
 
     for i in range(5000):
-        if(current_moon_year == moon_cycle_years and current_sun_year == sun_cycle_years):
+        if(current_moon_year == years_in_moon_cycle and current_sun_year == years_in_sun_cycle):
             print(i)
             break
 
-        current_moon_year = 1 if current_moon_year == moon_cycle_years else current_moon_year + 1
-        current_sun_year = 1 if current_sun_year == sun_cycle_years else current_sun_year + 1
-        
+        current_moon_year = 1 if current_moon_year == years_in_moon_cycle else current_moon_year + 1
+        current_sun_year = 1 if current_sun_year == years_in_sun_cycle else current_sun_year + 1
+       
 if __name__=="__main__":
     main()
